d4f55a4ae08d1ed66137e98aea3517e7e7498603
soqohm <soqohm@yandex.ru>
2024-03-17 11:16:12 +0500

    *
    *

--- 0.cs
+++ 0.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace AlgorithmsDataStructures
 {
@@ -44,34 +45,99 @@ namespace AlgorithmsDataStructures
         }
 
         // Печать только четных значений из списка
-        public static void PrintOnlyEvenDigits(LinkedList<int> list)
+
+        //public static void PrintOnlyEvenDigits(LinkedList<int> list) // предыдущая версия
+        //{
+        //    if (list.First is null) return;
+        //    if (list.First.Value % 2 == 0) Console.WriteLine(list.First.Value);
+        //    list.RemoveFirst();
+        //    PrintOnlyEvenDigits(list);
+        //}
+
+        public static void PrintOnlyEvenDigits(List<int> list) // поправил "список менять уже нельзя"
         {
-            if (list.First is null) return;
-            if (list.First.Value % 2 == 0) Console.WriteLine(list.First.Value);
-            list.RemoveFirst();
-            PrintOnlyEvenDigits(list);
+            void Print(int index)
+            {
+                if (index + 1 > list.Count) return;
+                if (list[index] % 2 == 0) Console.WriteLine(list[index]);
+                Print(index + 1);
+            }
+            Print(0);
         }
 
         // Печать элементов списка с четными индексами
-        public static void PrintOnlyEvenElements<T>(LinkedList<T> list)
+
+        //public static void PrintOnlyEvenElements<T>(LinkedList<T> list) // предыдущая версия
+        //{
+        //    void Print(LinkedListNode<T> node, bool isEvenIndex)
+        //    {
+        //        if (node is null) return;
+        //        if (isEvenIndex) Console.WriteLine(node.Value);
+        //        Print(node.Next, !isEvenIndex);
+        //    }
+        //    Print(list.First, false);
+        //}
+
+        public static void PrintOnlyEvenElements<T>(List<T> list) // поправил "одна проверка лишняя"
         {
-            void Print(LinkedListNode<T> node, bool isEvenIndex)
+            void Print(int index)
             {
-                if (node is null) return;
-                if (isEvenIndex) Console.WriteLine(node.Value);
-                Print(node.Next, !isEvenIndex);
+                if (index + 1 > list.Count) return;
+                if ((index + 1) % 2 == 0) Console.WriteLine(list[index]);
+                Print(index + 1);
             }
-            Print(list.First, false);
+            Print(0);
         }
 
-        // Нахождение второго максимального числа в списке
-        public static void Test7()
+        // Нахождение второго максимального числа в списке (с учетом, что максимальных может быть несколько)
+        public static List<int> FindSecondMaxValues(List<int> list)
         {
+            var maxValues = new List<int>();
+            var secondMaxValues = new List<int>();
+
+            void Find(int index)
+            {
+                if (index + 1 > list.Count) return;
+                SetMaxValues(list[index], ref maxValues, ref secondMaxValues);
+                Find(index + 1);
+            }
+            Find(0);
+            return secondMaxValues;
+        }
+
+        static void SetMaxValues(int value, ref List<int> maxValues, ref List<int> secondMaxValues)
+        {
+            if (maxValues.Count == 0)
+                maxValues = new List<int>() { value };
+            else if (value == maxValues[0])
+                maxValues.Add(value);
+            else if (value > maxValues[0])
+            {
+                secondMaxValues = maxValues;
+                maxValues = new List<int>() { value };
+            }
+            else if (secondMaxValues.Count == 0)
+                secondMaxValues = new List<int>() { value };
+            else if (value == secondMaxValues[0])
+                secondMaxValues.Add(value);
+            else if (value > secondMaxValues[0] && value < maxValues[0])
+                secondMaxValues = new List<int>() { value };
         }
 
         // Поиск всех файлов в заданном каталоге по всей иерархии вниз
-        public static void Test8()
+        public static List<string> RecursiveFileSearch(string startPath)
         {
+            var files = new List<string>();
+            if (!Directory.Exists(startPath)) return files;
+
+            void Find(string path)
+            {
+                files.AddRange(Directory.GetFiles(path));
+                foreach (var e in Directory.GetDirectories(path))
+                    Find(e);
+            }
+            Find(startPath);
+            return files;
         }
     }
 }
